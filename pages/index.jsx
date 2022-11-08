4f11322c356a2696946268a6b0af556a9ba26dbb
Ray Orolé <orole.ray@protonmail.com>
2022-11-08 21:57:14 +0100

    Change favicon

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,6 +4,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
+      <Head>
+        <link rel="shortcut icon" href="beafrans.png" type="image/png" />
+      </Head>
       <p className="text-red-500">hi bo frans</p>
     </div>
   );
