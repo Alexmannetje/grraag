ac2e93f327920daa56aa4d1aabcd032ff17bef6e
Alexander Mannaerts <Alexander.Mannaert@hotmail.com>
2022-11-08 22:08:38 +0100

    hi

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,10 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
     <div>
       <Head>
-        <link rel="shortcut icon" href="beafrans.png" type="image/png" />
+        <link rel="shortcut icon" href="bo.jpg" type="image/jpg" />
       </Head>
       <p className="text-red-500">hi bo frans</p>
     </div>
