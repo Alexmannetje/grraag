8bddf14a7f9867d73cf43a4d6673478120a7bb85
Alexander Mannaerts <AlexanderM507@lln.campussintursula.be>
2022-11-09 11:55:09 +0100

    nav

---
+++ components/nav2.jsx
@@ -0,0 +1,9 @@
+import react from "react";
+
+export default function Nav2() {
+  return (
+    <header aria-label="Site-Header" className="shadow-sm">
+      hiii
+    </header>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,15 @@
 import Head from "next/head";
-
-/* Import components */
 import Nav from "../components/nav";
+import Nav2 from "../components/nav2";
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <link rel="icon" type="image/ico" href="favicon.ico" />
+      </Head>
       <Nav />
+      <Nav2 />
       <p className="text-red-500">hi bo frans</p>
     </div>
   );
