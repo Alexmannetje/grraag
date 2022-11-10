929c54ad8614a439b14d4f574655540c48d5759f
Alexander Mannaerts <Alexander.Mannaert@hotmail.com>
2022-11-10 23:55:46 +0100

    nav

--- components/nav.jsx
+++ components/nav.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Nav() {
   return (
-    <nav class="bg-gray-700 border-gray-800 px-2 sm:px-4 py-2.5 rounded">
-      <div class="container flex flex-wrap justify-between items-center mx-auto">
-        <a href="https://flowbite.com/" class="flex items-center">
+    <nav class="bg-gray-700 py-2.5 mb-4 shadow-lg">
+      <div class="container flex flex-wrap justify-around items-center mx-40">
+        <a href="/" class="flex items-center mr-80">
           <img
             src="https://flowbite.com/docs/images/logo.svg"
             class="mr-3 h-6 sm:h-9"
@@ -18,9 +18,8 @@ export default function Nav() {
           <ul class="flex flex-col p-4 mt-4 bg-gray-800 rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 ">
             <li>
               <a
-                href="#"
-                class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-                aria-current="page"
+                href="/"
+                class="block py-2 pr-4 pl-3 text-gray-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Home
               </a>
@@ -28,31 +27,15 @@ export default function Nav() {
             <li>
               <a
                 href="#"
-                class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                About
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                class="block py-2 pr-4 pl-3 text-gray-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Services
               </a>
             </li>
             <li>
               <a
-                href="#"
-                class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Pricing
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                href="/contact"
+                class="block py-2 pr-4 pl-3 text-gray-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Contact
               </a>
--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Nav from "../components/nav";
 import Nav2 from "../components/nav2";
 import Footer from "../components/footer";
 
 export default function Contact() {
   return (
     <div className="h-screen bg-gray-700">
-      <Nav2 />
+      <Nav />
 
       <Footer />
     </div>
